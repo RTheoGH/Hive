6bdedd1a71eef2e5c8346d21be04507c28402f2e
RTheoGH <reynier.theo@gmail.com>
2024-01-16 15:48:07 +0100

    Quelques ajouts

--- public/script.js
+++ public/script.js
@@ -4,3 +4,16 @@ function clear(){
     $("body").removeClass();
     // faut jquery
 }
+
+// Fonction qui créé un hexagone
+function creeHexagone(rayon) {
+    var points = new Array();
+    for (var i = 0; i < 6; ++i) {
+        var angle = i * Math.PI / 3;
+        var x = Math.sin(angle) * rayon;
+        var y = -Math.cos(angle) * rayon;
+        //console.log("x="+Math.round(x*100)/100+" y="+Math.round(y*100)/100);
+        points.push([Math.round(x*100)/100, Math.round(y*100)/100]);
+    }
+    return points;
+}
