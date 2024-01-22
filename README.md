2755467eef065b0e7364501d3e6a6694d223f0ee
RTheoGH <reynier.theo@gmail.com>
2024-01-22 15:15:30 +0100

    Pour commencer !

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 ### JavaScript / Socket.IO / D3JS
 
 Hive est un jeu de stratégie pour deux joueurs, créé par John Yianni et édité par Gen42 en 2001. Véritable jeu d’échecs des temps modernes, Hive porte en lui des éléments qui font l’essence du jeu d’échecs : Simplicité et profondeur, pièces aux caractéristiques et déplacements uniques. Ce jeu ravira les Pousseurs de Bois les plus obtus. Si Magnus Carlsen renonce au Roi des Jeux, c’est qu’il batifole avec la « Reine Abeille » !
+
+---
+
+## Installer les packages
+```bash
+npm install
+```
+
+## Pour lancer le jeu 
+```bash
+node index.js
+```
--- public/script.js
+++ public/script.js
@@ -1,4 +1,10 @@
 /* fonction pour "clear" la page web afin d'afficher le jeu */
+
+function debutPartie(){
+    clear();
+    genereDamier(2,10,10);
+}
+
 function clear(){                    
     $(".menu").remove();
     $("body").removeClass();
@@ -16,4 +22,48 @@ function creeHexagone(rayon) {
         points.push([Math.round(x*100)/100, Math.round(y*100)/100]);
     }
     return points;
+}
+
+function genereDamier(rayon, nbLignes, nbColonnes) {
+    if(nbLignes==9 && nbColonnes==9){  /* augmente la taille globale du damier*/
+        rayon=rayon+5;
+    };
+    if(nbLignes==19 && nbColonnes==19){  /* reduire la taille globale du damier*/
+        rayon=rayon-5;
+    };
+    var i=0;
+    distance =  rayon - (Math.sin(1 * Math.PI / 3) * rayon);  // plus grande distance entre l'hexagone et le cercle circonscrit
+
+    d3.select("#tablier").append("svg").attr("width", (nbLignes*2)*2*rayon).attr("height",nbLignes*2*rayon);
+    var hexagone = creeHexagone(rayon);
+    for (var ligne=0; ligne < nbLignes; ligne++) {
+        i++;
+        for (var colonne=0; colonne < nbColonnes; colonne++) {
+            var d = "";
+            var x, y;
+            for (h in hexagone) {
+                x = hexagone[h][0]+(rayon-distance)*(2+i+2*colonne);
+                y = distance*2 + hexagone[h][1]+(rayon-distance*2)*(1+2*ligne);
+                if (h == 0) d += "M"+x+","+y+" L";
+                else        d +=     x+","+y+" ";
+            }
+            d += "Z";
+            d3.select("svg")
+                .append("path")
+                .attr("d", d)
+                .attr("stroke", "black")
+                .attr("fill", "white")
+                .attr("id", "h"+(ligne*nbLignes+colonne)) // car un id doit commencer par une lettre pour pouvoir être utilisé
+                //.on("click", function(d) {
+                    //let position=d3.select(this).attr('id').substring(1);
+                    //let typePion = document.querySelector('input[name="swap"]:checked').id;
+                    //console.log("typePion : "+typePion)
+                    //console.log(position);
+                    //socket.emit('pion',{'typePion':typePion,'position':position,'numJoueur':jeton});
+                    //console.log("typePion hexagone apres emit : "+typePion);
+                    // if(typePion=="pion")
+                    // d3.select(this).attr('fill', couleursJoueurs[jeton]);
+                //});
+            }
+    }
 }
