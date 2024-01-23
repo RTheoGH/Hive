0c987bb08399e952c5b065ecaee827323301e0a3
Killian <viguier.killian05@gmail.com>
2024-01-23 10:30:15 +0100

    Aggrandissement de la grille

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@
 
 function debutPartie(){
     clear();
-    genereDamier(50,10,10);
+    genereDamier(15,22);
 }
 
 function clear(){                    
@@ -24,25 +24,34 @@ function creeHexagone(rayon) {
     return points;
 }
 
-function genereDamier(rayon, nbLignes, nbColonnes) {
-    if(nbLignes==9 && nbColonnes==9){  /* augmente la taille globale du damier*/
+//idée : nbHexa = 0
+// for tailleCote :
+//  for tailleCote :
+//    ajouter nbHexa à gauche
+//    if nHexa
+
+
+function genereDamier(rayon, tailleCote) {
+    if(tailleCote==9 && tailleCote==9){  /* augmente la taille globale du damier*/
         rayon=rayon+5;
     };
-    if(nbLignes==19 && nbColonnes==19){  /* reduire la taille globale du damier*/
+    if(tailleCote==19 && tailleCote==19){  /* reduire la taille globale du damier*/
         rayon=rayon-5;
     };
     var i=0;
     distance =  rayon - (Math.sin(1 * Math.PI / 3) * rayon);  // plus grande distance entre l'hexagone et le cercle circonscrit
 
-    d3.select("#tablier").append("svg").attr("width", (nbLignes*2)*2*rayon).attr("height",nbLignes*2*rayon);
+    d3.select("#tablier").append("svg").attr("width", (tailleCote*2)*2*rayon).attr("height",tailleCote*2*rayon);
     var hexagone = creeHexagone(rayon);
-    for (var ligne=0; ligne < nbLignes; ligne++) {
+    console.log(hexagone)
+    var nbHexagoneLigne = 0
+    for (var ligne=0; ligne < tailleCote; ligne++) {
         i++;
-        for (var colonne=0; colonne < nbColonnes; colonne++) {
+        for (var colonne=0; colonne < tailleCote + nbHexagoneLigne; colonne++) {
             var d = "";
             var x, y;
             for (h in hexagone) {
-                x = hexagone[h][0]+(rayon-distance)*(2+i+2*colonne);
+                x = hexagone[h][0]+(rayon-distance)*(2+2*colonne-i)+(tailleCote*15);
                 y = distance*2 + hexagone[h][1]+(rayon-distance*2)*(1+2*ligne);
                 if (h == 0) d += "M"+x+","+y+" L";
                 else        d +=     x+","+y+" ";
@@ -53,7 +62,7 @@ function genereDamier(rayon, nbLignes, nbColonnes) {
                 .attr("d", d)
                 .attr("stroke", "black")
                 .attr("fill", "white")
-                .attr("id", "h"+(ligne*nbLignes+colonne)) // car un id doit commencer par une lettre pour pouvoir être utilisé
+                .attr("id", "h"+(ligne*tailleCote+colonne)) // car un id doit commencer par une lettre pour pouvoir être utilisé
                 //.on("click", function(d) {
                     //let position=d3.select(this).attr('id').substring(1);
                     //let typePion = document.querySelector('input[name="swap"]:checked').id;
@@ -65,5 +74,6 @@ function genereDamier(rayon, nbLignes, nbColonnes) {
                     // d3.select(this).attr('fill', couleursJoueurs[jeton]);
                 //});
             }
+        nbHexagoneLigne +=1
     }
 }
