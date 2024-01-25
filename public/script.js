4e912acba4d401adc860194b6554bd085bdd1ed9
Killian Viguier <killian.viguier@etu.umontpellier.fr>
2024-01-25 16:12:17 +0100

    Affichage des images des pions merci Discord

--- public/script.js
+++ public/script.js
@@ -115,20 +115,75 @@ function genereDamier(rayon, nbLignes, nbColonnes) {
 
     var d2 = "";
     for (h in hexagone) {
-        x = hexagone[h][0]+rayon;
-        y = hexagone[h][1]+rayon;
+        x = hexagone[h][0] + rayon;
+        y = hexagone[h][1] + rayon;
         if (h == 0) d2 += "M" + x + "," + y + " L";
         else d2 += x + "," + y + " ";
     }
     d2 += "Z";
-
+    
     // Ajouter l'hexagone à l'élément SVG dans #menuPions
     svgPions.append("path")
         .attr("d", d2)
         .attr("stroke", "black")
-        .attr("fill", "white")
-        .attr("id", "pionAbeille")
-        .attr("xlink:href", "/image/abeille.png");
+        .attr("fill", "white");
+    
+    console.log(document.getElementById("pionAbeille"));
+    var pionAb = d3.select('#pionAbeille')
+    pionAb.select('svg').append('image')
+        .attr('href', 'https://cdn.discordapp.com/attachments/1173320346372411485/1200083491887513642/abeille.png?ex=65c4e3d8&is=65b26ed8&hm=c3a5878cf857a8c4290650b43e743b82eecb5b953ee5d903b2121e8be1104b62&')
+        .attr('x', 10)  // Ajustez la position x de l'image à l'intérieur de l'hexagone
+        .attr('y', 10)  // Ajustez la position y de l'image à l'intérieur de l'hexagone
+        .attr('width', rayon*1.2)  // Ajustez la largeur de l'image
+        .attr('height', rayon*1.2);
+
+    var pionAr = d3.select('#pionAraignee')
+    pionAr.select('svg').append('image')
+        .attr('href', 'https://cdn.discordapp.com/attachments/1173320346372411485/1200083492164345976/araignee.png?ex=65c4e3d9&is=65b26ed9&hm=ff1c12d7ad6b268da2cc0fdc5060b1f7d7f7fe4c046c61e1fb153fb3ac79793d&')
+        .attr('x', 10)  // Ajustez la position x de l'image à l'intérieur de l'hexagone
+        .attr('y', 10)  // Ajustez la position y de l'image à l'intérieur de l'hexagone
+        .attr('width', rayon*1.2)  // Ajustez la largeur de l'image
+        .attr('height', rayon*1.2);
+
+    var pionCo = d3.select('#pionCoccinelle')
+    pionCo.select('svg').append('image')
+        .attr('href', 'https://cdn.discordapp.com/attachments/1173320346372411485/1200083492424384672/coccinelle.png?ex=65c4e3d9&is=65b26ed9&hm=7ee727fe64fafdb8b90c1ab6c52958debe59d15b9939e2f94f2c2fe6cb192f42&')
+        .attr('x', 10)  // Ajustez la position x de l'image à l'intérieur de l'hexagone
+        .attr('y', 10)  // Ajustez la position y de l'image à l'intérieur de l'hexagone
+        .attr('width', rayon*1.2)  // Ajustez la largeur de l'image
+        .attr('height', rayon*1.2);
+    
+    var pionFo = d3.select('#pionFourmi')
+    pionFo.select('svg').append('image')
+        .attr('href', 'https://cdn.discordapp.com/attachments/1173320346372411485/1200083492755742800/fourmi.png?ex=65c4e3d9&is=65b26ed9&hm=6a385770c2fde61c2803090fb2ba4547db12abfa4cb0c88ed539d8698a498856&')
+        .attr('x', 10)  // Ajustez la position x de l'image à l'intérieur de l'hexagone
+        .attr('y', 10)  // Ajustez la position y de l'image à l'intérieur de l'hexagone
+        .attr('width', rayon*1.2)  // Ajustez la largeur de l'image
+        .attr('height', rayon*1.2);
+
+    var pionMo = d3.select('#pionMoustique')
+    pionMo.select('svg').append('image')
+        .attr('href', 'https://cdn.discordapp.com/attachments/1173320346372411485/1200083492994814012/moustique.png?ex=65c4e3d9&is=65b26ed9&hm=0818af4e00bf1abdfb89f2a4d363db90c18e46f88faea801b44a93bfeb4394ed&')
+        .attr('x', 10)  // Ajustez la position x de l'image à l'intérieur de l'hexagone
+        .attr('y', 10)  // Ajustez la position y de l'image à l'intérieur de l'hexagone
+        .attr('width', rayon*1.2)  // Ajustez la largeur de l'image
+        .attr('height', rayon*1.2);
+
+    var pionSa = d3.select('#pionSauterelle')
+    pionSa.select('svg').append('image')
+        .attr('href', 'https://cdn.discordapp.com/attachments/1173320346372411485/1200083493225496636/sauterelle.png?ex=65c4e3d9&is=65b26ed9&hm=82cbd9cb1cc8362d85c0f38cabe98eb076b9769ebac5548ebc15312535097a28&')
+        .attr('x', 10)  // Ajustez la position x de l'image à l'intérieur de l'hexagone
+        .attr('y', 10)  // Ajustez la position y de l'image à l'intérieur de l'hexagone
+        .attr('width', rayon*1.2)  // Ajustez la largeur de l'image
+        .attr('height', rayon*1.2);
+
+    var pionSc = d3.select('#pionScarabee')
+    pionSc.select('svg').append('image')
+        .attr('href', 'https://cdn.discordapp.com/attachments/1173320346372411485/1200083493556850758/scarabee.png?ex=65c4e3d9&is=65b26ed9&hm=2131d68f3b5b2b2ce4c06e679ae90111accdb11d123d6c1479f3d2fc539db1c5&')
+        .attr('x', 10)  // Ajustez la position x de l'image à l'intérieur de l'hexagone
+        .attr('y', 10)  // Ajustez la position y de l'image à l'intérieur de l'hexagone
+        .attr('width', rayon*1.2)  // Ajustez la largeur de l'image
+        .attr('height', rayon*1.2);
 
     d3.select('#h5250').attr('fill', 'red')
     d3.select('#h5249').attr('fill', 'green')
