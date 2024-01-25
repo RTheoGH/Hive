69073f5074361ddd2f8fe70a8d7e5f857f467f46
Killian <viguier.killian05@gmail.com>
2024-01-25 22:09:27 +0100

    Merge du front de la branche theo-front

--- public/script.js
+++ public/script.js
@@ -7,12 +7,12 @@ $(document).ready(() => {
 /* fonction pour "clear" la page web afin d'afficher le jeu */
 function debutPartie(){
     clear();
-    genereDamier(25,100,100);
+    $("#jeu").show();
+    genereDamier(40,40,40);
 }
 
 function clear(){                    
-    $(".menu").remove();
-    $("body").removeClass();
+    $(".menu").hide();
 }
 
 function creer(){
@@ -43,6 +43,8 @@ function validerRejoindre(){
 
 function quitter(){
     $("#lobby").hide();
+    $("#jeu").hide();
+    $(".menu").show();
     $("#accueil").show();
 }
 
@@ -93,7 +95,7 @@ function genereDamier(rayon, nbLignes, nbColonnes) {
                 .append("path")
                 .attr("d", d)
                 .attr("stroke", "black")
-                .attr("fill", "white")
+                .attr("fill", "burlywood")
                 .attr("id", "h"+(ligne*nbLignes+colonne)) // car un id doit commencer par une lettre pour pouvoir être utilisé
                 .on("click", function() {
                     //let position=d3.select(this).attr('id').substring(1);
@@ -134,56 +136,56 @@ function genereDamier(rayon, nbLignes, nbColonnes) {
         .attr('href', 'https://cdn.discordapp.com/attachments/1173320346372411485/1200083491887513642/abeille.png?ex=65c4e3d8&is=65b26ed8&hm=c3a5878cf857a8c4290650b43e743b82eecb5b953ee5d903b2121e8be1104b62&')
         .attr('x', 10)  // Ajustez la position x de l'image à l'intérieur de l'hexagone
         .attr('y', 10)  // Ajustez la position y de l'image à l'intérieur de l'hexagone
-        .attr('width', rayon*1.2)  // Ajustez la largeur de l'image
-        .attr('height', rayon*1.2);
+        .attr('width', rayon*1.3)  // Ajustez la largeur de l'image
+        .attr('height', rayon*1.3);
 
     var pionAr = d3.select('#pionAraignee')
     pionAr.select('svg').append('image')
         .attr('href', 'https://cdn.discordapp.com/attachments/1173320346372411485/1200083492164345976/araignee.png?ex=65c4e3d9&is=65b26ed9&hm=ff1c12d7ad6b268da2cc0fdc5060b1f7d7f7fe4c046c61e1fb153fb3ac79793d&')
         .attr('x', 10)  // Ajustez la position x de l'image à l'intérieur de l'hexagone
         .attr('y', 10)  // Ajustez la position y de l'image à l'intérieur de l'hexagone
-        .attr('width', rayon*1.2)  // Ajustez la largeur de l'image
-        .attr('height', rayon*1.2);
+        .attr('width', rayon*1.3)  // Ajustez la largeur de l'image
+        .attr('height', rayon*1.3);
 
     var pionCo = d3.select('#pionCoccinelle')
     pionCo.select('svg').append('image')
         .attr('href', 'https://cdn.discordapp.com/attachments/1173320346372411485/1200083492424384672/coccinelle.png?ex=65c4e3d9&is=65b26ed9&hm=7ee727fe64fafdb8b90c1ab6c52958debe59d15b9939e2f94f2c2fe6cb192f42&')
         .attr('x', 10)  // Ajustez la position x de l'image à l'intérieur de l'hexagone
         .attr('y', 10)  // Ajustez la position y de l'image à l'intérieur de l'hexagone
-        .attr('width', rayon*1.2)  // Ajustez la largeur de l'image
-        .attr('height', rayon*1.2);
+        .attr('width', rayon*1.3)  // Ajustez la largeur de l'image
+        .attr('height', rayon*1.3);
     
     var pionFo = d3.select('#pionFourmi')
     pionFo.select('svg').append('image')
         .attr('href', 'https://cdn.discordapp.com/attachments/1173320346372411485/1200083492755742800/fourmi.png?ex=65c4e3d9&is=65b26ed9&hm=6a385770c2fde61c2803090fb2ba4547db12abfa4cb0c88ed539d8698a498856&')
         .attr('x', 10)  // Ajustez la position x de l'image à l'intérieur de l'hexagone
         .attr('y', 10)  // Ajustez la position y de l'image à l'intérieur de l'hexagone
-        .attr('width', rayon*1.2)  // Ajustez la largeur de l'image
-        .attr('height', rayon*1.2);
+        .attr('width', rayon*1.3)  // Ajustez la largeur de l'image
+        .attr('height', rayon*1.3);
 
     var pionMo = d3.select('#pionMoustique')
     pionMo.select('svg').append('image')
         .attr('href', 'https://cdn.discordapp.com/attachments/1173320346372411485/1200083492994814012/moustique.png?ex=65c4e3d9&is=65b26ed9&hm=0818af4e00bf1abdfb89f2a4d363db90c18e46f88faea801b44a93bfeb4394ed&')
         .attr('x', 10)  // Ajustez la position x de l'image à l'intérieur de l'hexagone
         .attr('y', 10)  // Ajustez la position y de l'image à l'intérieur de l'hexagone
-        .attr('width', rayon*1.2)  // Ajustez la largeur de l'image
-        .attr('height', rayon*1.2);
+        .attr('width', rayon*1.3)  // Ajustez la largeur de l'image
+        .attr('height', rayon*1.3);
 
     var pionSa = d3.select('#pionSauterelle')
     pionSa.select('svg').append('image')
         .attr('href', 'https://cdn.discordapp.com/attachments/1173320346372411485/1200083493225496636/sauterelle.png?ex=65c4e3d9&is=65b26ed9&hm=82cbd9cb1cc8362d85c0f38cabe98eb076b9769ebac5548ebc15312535097a28&')
         .attr('x', 10)  // Ajustez la position x de l'image à l'intérieur de l'hexagone
         .attr('y', 10)  // Ajustez la position y de l'image à l'intérieur de l'hexagone
-        .attr('width', rayon*1.2)  // Ajustez la largeur de l'image
-        .attr('height', rayon*1.2);
+        .attr('width', rayon*1.3)  // Ajustez la largeur de l'image
+        .attr('height', rayon*1.3);
 
     var pionSc = d3.select('#pionScarabee')
     pionSc.select('svg').append('image')
         .attr('href', 'https://cdn.discordapp.com/attachments/1173320346372411485/1200083493556850758/scarabee.png?ex=65c4e3d9&is=65b26ed9&hm=2131d68f3b5b2b2ce4c06e679ae90111accdb11d123d6c1479f3d2fc539db1c5&')
         .attr('x', 10)  // Ajustez la position x de l'image à l'intérieur de l'hexagone
         .attr('y', 10)  // Ajustez la position y de l'image à l'intérieur de l'hexagone
-        .attr('width', rayon*1.2)  // Ajustez la largeur de l'image
-        .attr('height', rayon*1.2);
+        .attr('width', rayon*1.3)  // Ajustez la largeur de l'image
+        .attr('height', rayon*1.3);
 
     d3.select('#h5250').attr('fill', 'red')
     d3.select('#h5249').attr('fill', 'green')
